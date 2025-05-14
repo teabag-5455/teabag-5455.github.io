1cdfd1ac2d9914426def44e86a8e0405ee55b373
teabag-5455 <148552008+teabag-5455@users.noreply.github.com>
2025-05-15 00:21:06 +0800

    Add files via upload

---
+++ blob.js
@@ -0,0 +1,68 @@
+const MIN_SPEED = 0.5
+const MAX_SPEED = 3
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min
+}
+
+class Blob {
+  constructor(el) {
+    this.el = el
+    const boundingRect = this.el.getBoundingClientRect()
+    this.size = boundingRect.width
+    // 隨機初始位置
+    this.initialX = randomNumber(0, window.innerWidth - this.size)
+    this.initialY = randomNumber(0, window.innerHeight - this.size)
+    this.el.style.top = `${this.initialY}px`
+    this.el.style.left = `${this.initialX}px`
+    // 速度
+    this.vx =
+      randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1)
+    this.vy =
+      randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1)
+    this.x = this.initialX
+    this.y = this.initialY
+  }
+
+  update() {
+    this.x += this.vx
+    this.y += this.vy
+    if (this.x >= window.innerWidth - this.size) {
+      this.x = window.innerWidth - this.size
+      this.vx *= -1
+    }
+    if (this.y >= window.innerHeight - this.size) {
+      this.y = window.innerHeight - this.size
+      this.vy *= -1
+    }
+    if (this.x <= 0) {
+      this.x = 0
+      this.vx *= -1
+    }
+    if (this.y <= 0) {
+      this.y = 0
+      this.vy *= -1
+    }
+
+    this.el.style.transform =
+      `translate(${this.x - this.initialX}px, ${this.y - this.initialY
+      }px)`
+  }
+}
+
+function initBlobs() {
+  const blobEls = document.querySelectorAll('.blob')
+  const blobs = Array.from(blobEls).map((blobEl) => new Blob(blobEl))
+
+  function update() {
+    requestAnimationFrame(update)
+    blobs.forEach((blob) => blob.update()
+    )
+  }
+  requestAnimationFrame(update)
+}
+
+initBlobs()
+
+
+
